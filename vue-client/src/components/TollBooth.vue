c105647983b8a39f241849cc1ee63b3179698465
Sebastian Gomes <romy6047@gmail.com>
2025-03-10 15:03:29 -0600

    toll booth banner added

---
+++ vue-client/src/components/TollBooth.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  boothCount: number
+  header: string
+}>()
+</script>
+<template>
+  <div class="mt-6 w-full px-3">
+    <div class="bg-green-700 text-white rounded-lg p-1 text-center">
+      <div class="border-white border-4 rounded-lg p-4">
+        <h2 class="text-4xl font-bold">{{ boothCount }}</h2>
+        <p class="text-3xl mt-5">{{ header }}</p>
+      </div>
+    </div>
+  </div>
+</template>
--- vue-client/src/views/Dashboard.vue
+++ vue-client/src/views/Dashboard.vue
@@ -1,1 +1,12 @@
-<template>Dashboard</template>
+<script setup lang="ts">
+import TollBooth from '@/components/TollBooth.vue'
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <TollBooth :booth-count="1" :header="'Semi Trucks'"></TollBooth>
+    <TollBooth :booth-count="2" :header="'Sedans and Suv'"></TollBooth>
+    <TollBooth :booth-count="3" :header="'Sedans and Suv'"></TollBooth>
+    <TollBooth :booth-count="4" :header="'Emergency Responders'"></TollBooth>
+  </div>
+</template>
